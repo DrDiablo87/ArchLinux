724832fa5bff655ffd8ec270ff0704e2012e0c7a
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2021-04-13 17:29:25 +0300

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -31,7 +31,6 @@ yay -S --mflags --skipinteg --noconfirm mkvtoolnix-gui ventoy-bin timeshift mega
 yay -U --mflags --skipinteg --noconfirm ~/ArchLinux/Package/mystiq-20.03.23-1-x86_64.pkg.tar.zst
 
 sudo systemctl enable fail2ban.service
-yay -U --mflags --skipinteg --noconfirm ~/ArchLinux/Package/mesen-0.9.9-2-x86_64.pkg.tar.zst
 yay -S --mflags --skipinteg --noconfirm libreoffice-fresh-ru unoconv
 
 yes | sudo sensors-detect
