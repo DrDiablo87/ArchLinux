46da9a928253eff32eb56c6b057a6b749809599b
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2021-04-15 23:26:20 +0300

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -25,10 +25,11 @@ fi
 #===========================================================================================
 yay -S --mflags --skipinteg --noconfirm profile-sync-daemon && psd -p
 systemctl --user enable psd.service
-yay -S --mflags --skipinteg --noconfirm archiso netactview loudmouth hddtemp plasma5-applets-systemd plasma5-applets-thermal-monitor plasma5-applets-resources-monitor plasma5-applets-eventcalendar netdiscover fail2ban plasma5-applets-netspeed ffmulticonverter steghide
+yay -S --mflags --skipinteg --noconfirm archiso netactview loudmouth hddtemp plasma5-applets-systemd plasma5-applets-thermal-monitor plasma5-applets-eventcalendar netdiscover fail2ban plasma5-applets-netspeed ffmulticonverter steghide
 yay -S --mflags --skipinteg --noconfirm airgeddon metasploit dhcp hashcat hashcat-utils ipscan crunch mdk4 reaver beef hostapd lighttpd bettercap ettercap sslstrip dsniff bully pixiewps usbutils xorg-xdpyinfo ccze asleap john hostapd-wpe nftables mdk3 hcxtools hcxdumptool
 yay -S --mflags --skipinteg --noconfirm mkvtoolnix-gui ventoy-bin timeshift megasync fritzing mednaffe mednafen
 yay -U --mflags --skipinteg --noconfirm ~/ArchLinux/Package/mystiq-20.03.23-1-x86_64.pkg.tar.zst
+#yay -S --mflags --skipinteg --noconfirm plasma5-applets-resources-monitor 
 
 sudo systemctl enable fail2ban.service
 yay -S --mflags --skipinteg --noconfirm libreoffice-fresh-ru unoconv
