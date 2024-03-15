20642870b3e1736c5ef0e92ad58bf86b4af3a916
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2024-03-15 18:58:47 +0300

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -161,9 +161,9 @@ sed -i 's/#MAKEFLAGS="-j'$(nproc)'"/MAKEFLAGS="-j'$(nproc)'"/g' /etc/makepkg.con
 #INTEL
 #pacman -S --needed lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
 
-###pacman -S xorg-drivers nvidia xorg-xinit mesa-vdpau lib32-mesa plasma --noconfirm
+pacman -S xorg-drivers nvidia xorg-xinit mesa-vdpau lib32-mesa plasma --noconfirm
 #pacman -S nvidia-settings nvidia-settings xorg-server-devel opencl-nvidia nvidia && wget https://ru.download.nvidia.com/XFree86/Linux-x86_64/390.141/NVIDIA-Linux-x86_64-390.141.run
-pacman -S plasma-meta xorg sddm sddm-kcm networkmanager --noconfirm
+pacman -S plasma-workspace xorg sddm sddm-kcm networkmanager --noconfirm
 pacman -S yay --noconfirm
 systemctl enable sddm.service
 systemctl enable NetworkManager.service
