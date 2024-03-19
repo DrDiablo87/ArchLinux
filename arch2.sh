12ceb1a67e8ecbc47db5f43389ced386426040da
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2024-03-20 00:37:26 +0300

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -166,7 +166,7 @@ pacman -S --needed lib32-mesa mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-
 #INTEL
 #pacman -S --needed lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
 
-pacman -S systemd xorg-drivers nvidia xorg-xinit mesa-vdpau lib32-mesa --noconfirm
+pacman -S xorg-drivers nvidia xorg-xinit mesa-vdpau lib32-mesa --noconfirm
 #pacman -S nvidia-settings nvidia-settings xorg-server-devel opencl-nvidia nvidia && wget https://ru.download.nvidia.com/XFree86/Linux-x86_64/390.141/NVIDIA-Linux-x86_64-390.141.run
 pacman -S plasma-workspace xorg sddm sddm-kcm networkmanager --noconfirm
 pacman -S yay --noconfirm
