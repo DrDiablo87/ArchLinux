def04ffafb2ed169f301d363db0dd46d923144c0
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2024-04-01 10:44:59 +0300

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -240,3 +240,153 @@ systemctl mask systemd-tmpfiles-setup.service    # предотвращение 
 btrfs subvolume delete /var/lib/machines         # удаление проблемного снапшота
 usermod -a -G wireshark $username
 
+
+#=======================================================================================================================================================================================================+++++++
+#Настраиваем тему
+mkdir /etc/sddm.conf.d                           # Автологин
+echo '[Autologin]
+User='$username'
+Session=plasma.desktop
+Numlock=on ' > /etc/sddm.conf.d/autologin.conf
+
+echo '[Autologin]
+Relogin=false
+Session=plasma
+User='$username'
+
+[General]
+HaltCommand=/usr/bin/systemctl poweroff
+RebootCommand=/usr/bin/systemctl reboot
+
+[Theme]
+Current=breeze
+CursorTheme=breeze_cursors
+
+[Users]
+MaximumUid=60000
+MinimumUid=1000 ' > /etc/sddm.conf
+
+
+echo -e '
+
+\e[31m==================================================================================== Установка и настройка MPV ===================================\e[0m
+'
+echo -e '\033[32m'
+pacman -U /home/$username/ArchLinux/Package/arc-kde-git-220180614.r11.g04873ca-1-any.pkg.tar.xz --noconfirm
+#cp /home/$username/ArchLinux/Package/amarok.mo /usr/share/locale/ru/LC_MESSAGES/amarok.mo
+cp /home/$username/ArchLinux/Package/steghide-kdialog /usr/bin/steghide-kdialog
+chmod +x /usr/bin/steghide-kdialog
+pacman -S amarok gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly --noconfirm
+
+pacman -S mpv --noconfirm
+cp /home/$username/ArchLinux/Package/mpv.desktop /usr/share/applications/mpv.desktop
+cp /home/$username/ArchLinux/Package/mpv_single /usr/bin/mpv_single
+chmod +x /usr/bin/mpv_single
+cp /home/$username/ArchLinux/Package/mpv.conf /etc/mpv/mpv.conf
+cp /home/$username/ArchLinux/Package/input.conf /etc/mpv/input.conf
+
+echo -e '
+
+\e[31m==================================================================================== Установка и настройка Live Wallpaper ========================\e[0m
+'
+echo -e '\033[32m'
+pacman -S mplayer libva-vdpau-driver libva-mesa-driver libva-intel-driver libvdpau-va-gl lib32-libva-vdpau-driver lib32-mesa-vdpau lib32-libva-mesa-driver --noconfirm
+#pacman -U /home/$username/ArchLinux/Package/shantz-xwinwrap-bzr-20090421-3-x86_64.pkg.tar.xz  --noconfirm
+mkdir /home/$username/.config/autostart-scripts
+mkdir /home/$username/.config/LiveWallpaper
+#echo "#!/bin/bash
+#xwinwrap -ni -fs -s -st -sp -b -nf -- mplayer -fps 25 -framedrop -wid WID -nosound "~/.config/LiveWallpaper/Galaxy.mp4" -loop 0
+#" > /home/$username/.config/LiveWallpaper/LiveWallpaper
+#chmod +x /home/$username/.config/LiveWallpaper/LiveWallpaper
+#touch /etc/systemd/system/Livewallpaper.service
+#chmod 664 /etc/systemd/system/Livewallpaper.service
+#echo "[Unit]
+#Description=LiveWallpaper
+
+#[Service]
+#Type=simple
+#User=$username
+#ExecStart=/bin/bash -c 'DISPLAY=:0 ~/.config/LiveWallpaper/LiveWallpaper'
+
+#[Install]
+#WantedBy=multi-user.target " > /etc/systemd/system/Livewallpaper.service
+
+#echo "#!/bin/bash
+#sudo systemctl start livewallpaper.service
+#" > /home/$username/.config/autostart-scripts/LiveWallpaperService
+#chmod +x /home/$username/.config/autostart-scripts/LiveWallpaperService
+
+#=========================================================
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=15h4BUSE7AN9n0s_eUMd06LbkYn_9pmQM' -O /home/$username/.config/LiveWallpaper/Galaxy.mp4
+echo '#brightness=-5
+#vo=vdpau,
+#vc=ffh264vdpau,ffmpeg12vdpau,ffodivxvdpau,ffwmv3vdpau,ffvc1vdpau,ffhevcvdpau
+brightness=-10
+contrast=10
+saturation=10 '> /etc/mplayer/mplayer.conf
+cp /home/$username/ArchLinux/LiveWallpaper/* /home/$username/.config/LiveWallpaper
+cp /home/$username/.config/LiveWallpaper/screenshot.jpg /usr/share/sddm/themes/breeze/preview.png
+cp /home/$username/.config/LiveWallpaper/screenshot.jpg /usr/share/sddm/themes/breeze/screenshot.jpg
+cp /home/$username/.config/LiveWallpaper/archlinux-logo-dark.png /usr/share/sddm/themes/breeze/archlinux-logo-dark.png
+rm -r /usr/share/wallpapers/Next/contents/
+
+echo "[General]
+background=screenshot.jpg
+type=image " > /usr/share/sddm/themes/breeze/theme.conf.user
+
+cp /home/$username/ArchLinux/Package/theme.conf /usr/share/sddm/themes/breeze/theme.conf
+
+#Настройка Dolphin
+
+cp /home/$username/ArchLinux/Package/Dolphin-Root.desktop /usr/share/kservices5/ServiceMenus/Dolphin-Root.desktop
+cp /home/$username/ArchLinux/Package/steghide.desktop /usr/share/kservices5/ServiceMenus/steghide.desktop
+
+pacman -S clamav --noconfirm
+echo 'PidFile /run/clamav/freshclam.pid
+DatabaseMirror https://packages.microsoft.com/clamav/
+NotifyClamd /etc/clamav/clamd.conf' > /etc/clamav/freshclam.conf
+freshclam
+cp /home/$username/ArchLinux/Package/ClamAV.desktop /usr/share/kservices5/ServiceMenus/ClamAV.desktop
+cp /home/$username/ArchLinux/Package/clamav.svg /usr/share/icons/hicolor/scalable/apps/clamav.svg
+
+#SWAP
+
+#truncate -s 0 /swapfile
+#chattr +C /swapfile
+#fallocate -l 512M /swapfile
+#chmod 600 /swapfile
+#mkswap /swapfile
+#swapon /swapfile
+#echo /swapfile none swap sw 0 0 | sudo tee -a /etc/fstab
+#echo 'vm.swappiness=10' > /etc/sysctl.d/99-sysctl.conf
+
+rm -r /usr/share/plasma/desktoptheme/Arc-Dark/icons
+cp -r /usr/share/plasma/desktoptheme/default/icons /usr/share/plasma/desktoptheme/Arc-Dark
+rm -r /usr/share/plasma/desktoptheme/Arc-Dark/widgets
+cp -r /usr/share/plasma/desktoptheme/default/widgets /usr/share/plasma/desktoptheme/Arc-Dark
+cp /usr/share/icons/breeze/apps/48/plasmavault.svg /usr/share/icons/breeze/apps/48/kleopatra.svg  
+cp /usr/share/icons/breeze/apps/48/plasmavault.svg /usr/share/icons/breeze-dark/apps/48/kleopatra.svg
+cp /usr/share/icons/breeze/preferences/32/preferences-desktop-keyboard.svg /usr/share/icons/breeze/preferences/32/qvkbd.svg
+cp /usr/share/icons/breeze-dark/preferences/32/preferences-desktop-keyboard.svg /usr/share/icons/breeze-dark/preferences/32/qvkbd.svg
+sed -i 's/Icon=kleopatra/Icon=plasmavault/g' /usr/share/kservices5/kleopatra_decryptverifyfiles.desktop /usr/share/kservices5/kleopatra_decryptverifyfolders.desktop /usr/share/kservices5/kleopatra_signencryptfiles.desktop /usr/share/kservices5/kleopatra_signencryptfolders.desktop
+sed -i 's/use-ipv4=yes/use-ipv4=no/g' /etc/avahi/avahi-daemon.conf
+sed -i 's/use-ipv6=yes/use-ipv6=no/g' /etc/avahi/avahi-daemon.conf
+cp /home/$username/ArchLinux/Package/systemdgenie.mo /usr/share/locale/ru/LC_MESSAGES/systemdgenie.mo
+cp /home/$username/ArchLinux/arch3.sh /home/$username/1
+
+echo "#!/bin/bash
+sleep 10
+konsole -e sh ./1" > /home/$username/.config/autostart-scripts/xxx
+chmod +x /home/$username/.config/autostart-scripts/xxx
+
+echo "
+tmpfs						/var/log	tmpfs	defaults,noatime 0 0
+tmpfs						/var/run	tmpfs	defaults,noatime 0 0
+tmpfs						/var/lock	tmpfs	defaults,noatime 0 0 " >> /etc/fstab
+
+rm -R /home/$username/.bash_logout /home/$username/.bash_profile /home/$username/.bashrc /home/$username/Package /var/cache/pacman/pkg /boot/initramfs-linux-fallback.img
+chown -R $username:users /home/$username
+
+cp -Rf /home/$username/ArchLinux/KDE/.config/kscreenlockerrc /home/$username/.config
+cp -Rf /home/$username/ArchLinux/KDE/. /root
+
