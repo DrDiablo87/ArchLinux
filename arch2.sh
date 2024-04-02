7f802b4e79972667a823d48c5bd32b6cd0d57e54
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2024-04-02 16:08:09 +0300

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -273,10 +273,10 @@ systemctl mask systemd-journald.socket
 ##systemctl mask systemd-journald.service
 systemctl mask systemd-journald-audit.socket
 systemctl mask systemd-journald-dev-log.socket
-#systemctl enable fstrim.service
-#systemctl enable fstrim.timer
+systemctl enable fstrim.service
+systemctl enable fstrim.timer
 systemctl mask systemd-tmpfiles-setup.service    # предотвращение создания проблемного снапшота
-btrfs subvolume delete /var/lib/machines         # удаление проблемного снапшота
+#btrfs subvolume delete /var/lib/machines         # удаление проблемного снапшота
 usermod -a -G wireshark $username
 
 
