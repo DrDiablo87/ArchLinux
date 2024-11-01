c1fd07c2bbec6892416c846833284c4c1d5fba18
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2024-11-01 06:31:51 +0300

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -82,4 +82,4 @@ sudo tar -xf ~/ArchLinux/Package/archlive.tar.gz -C ~/
 sudo rm -R ~/1 ~/.config/autostart-scripts/xxx ~/.config/autostart/xxx 
 qdbus org.kde.ksmserver /KSMServer logout 0 1 2  #logout 0 3 3
 rm ~/.config/autostart/arch3.sh.desktop ~/.config/autostart/restart
-#systemctl reboot
+systemctl reboot
