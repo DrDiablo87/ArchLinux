b5c04201a445bf6f0149a73495c47437fa8bc140
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2025-08-05 21:07:08 +0300

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -27,6 +27,7 @@ fi
 #===========================================================================================
 git config --global url."https://".insteadOf git://
 yay -S --mflags --skipinteg --noconfirm profile-sync-daemon noisy-py3-git 
+sudo ln /usr/lib/systemd/user/psd.service /usr/lib/systemd/user/Firefox.service
 sudo mv /etc/systemd/system/noisy.service /etc/systemd/system/Noisy.service
 sudo mv /usr/share/noisy/examples/systemd/noisy.service /usr/share/noisy/examples/systemd/Noisy.service
 ## psd -p
