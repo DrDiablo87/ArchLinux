6bf2446b1c95dc99b9cabb57644e4087b9b7f1fb
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2025-11-12 03:50:00 +0300

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,53 +1,61 @@
 #!/bin/bash
+# quick-install.sh - Еще более минималистичная версия
 
-loadkeys ru
-setfont cyr-sun16
+set -e
 
-#Синхронизация системных часов
-timedatectl set-ntp true
+DISK="/dev/nvme0n1"
 
-echo -e '\033[32m'
-read -p "Введите имя компьютера: " hostname
-read -p "Введите имя пользователя: " username
-read -p "Введите пароль root " rootpass
-read -p "Введите пароль user " userpass
-export hostname username rootpass userpass
+# Разметка
+wipefs -a $DISK
+parted -s $DISK mklabel gpt
+parted -s $DISK mkpart "EFI" fat32 1MiB 513MiB
+parted -s $DISK set 1 esp on
+parted -s $DISK mkpart "root" 513MiB 100%
 
-echo -e '\033[32m' &&
+mkfs.fat -F32 "${DISK}p1"
+mkfs.btrfs -f "${DISK}p2"
 
-#dd if=/dev/zero of=/dev/sda bs=1024 count=50
-wipefs -a /dev/sda
-
-#Ваша разметка диска
-echo -e '\e[31m' ; lsblk -f
-
-#Форматирование дисков
-mkfs.btrfs -L "Arch" -n 64k /dev/sda
-
-#Монтирование дисков
-mount -o compress=lzo /dev/sda /mnt
-cd /mnt
-btrfs subvolume create @
-cd /
+mount "${DISK}p2" /mnt
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
 umount /mnt
-mount -t btrfs -o noatime,nodatasum,compress=lzo,ssd,max_inline=0,subvol=@ /dev/sda /mnt
 
+mount -o subvol=@ "${DISK}p2" /mnt
+mkdir -p /mnt/{home,boot}
+mount -o subvol=@home "${DISK}p2" /mnt/home
+mount "${DISK}p1" /mnt/boot
 
-#Выбор зеркал для загрузки
-echo "Server = https://mirror.yandex.ru/archlinux/\$repo/os/\$arch
-Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+# Установка
+pacstrap /mnt base linux linux-firmware btrfs-progs sudo
+genfstab -U /mnt >> /mnt/etc/fstab
 
-echo -e '
-\e[31m==================================================================================== Установка основных пакетов ==================================\e[0m
-'
-echo -e '\033[32m'
-pacstrap /mnt base nano linux #linux-firmware base-devel
+# Chroot конфигурация
+arch-chroot /mnt bash << EOF
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+hwclock --systohc
+echo "LANG=ru_RU.UTF-8" > /etc/locale.conf
+echo "KEYMAP=ru" > /etc/vconsole.conf
+echo "arch" > /etc/hostname
 
-echo -e '
-\e[31m==================================================================================== Настройка системы ===========================================\e[0m
-'
-echo -e '\033[32m'
-genfstab -pU /mnt >> /mnt/etc/fstab
+echo "root:123456" | chpasswd
+useradd -m -G wheel -s /bin/bash user
+echo "user:123456" | chpasswd
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
-arch-chroot /mnt sh -c "$(curl -fsSL git.io/www1.sh)"
-systemctl reboot
+bootctl --path=/boot install
+echo "default arch" > /boot/loader/loader.conf
+echo "timeout 3" >> /boot/loader/loader.conf
+
+cat > /boot/loader/entries/arch.conf << EOL
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=PARTUUID=$(blkid -s PARTUUID -o value ${DISK}p2) rw rootflags=subvol=@
+EOL
+
+# Минимальный KDE
+pacman -S --noconfirm plasma-desktop sddm dolphin konsole firefox
+systemctl enable sddm NetworkManager
+EOF
+
+echo "Установка завершена!"
