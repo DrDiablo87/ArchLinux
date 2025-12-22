6019f8eb8190b142752fbb8f14051d9721841ba4
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2025-12-22 14:01:58 +0300

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -41,11 +41,7 @@ echo -e '
 '
 echo -e '\033[32m'
 pacman -S efibootmgr git wget reflector amd-ucode iwd --noconfirm 
-echo '[General]
-EnableNetworkConfiguration=true
 
-[Network]
-NameResolvingService=systemd' > /etc/iwd/main.conf
 
 pacman -Syu --noconfirm
 pacman -S grub --noconfirm
