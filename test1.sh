f2e07ccae2eb88ad0c0797df8e55dbeffdad343a
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2025-12-23 14:22:36 +0300

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -70,10 +70,10 @@ Defaults env_reset, timestamp_timeout=30' > /etc/sudoers
 git clone https://github.com/DrDiablo87/ArchLinux.git /home/$username/ArchLinux
 ping -c 5 google.com
 
-#pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-#pacman-key --lsign-key 3056513887B78AEB
-#pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'  --noconfirm
-#pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'  --noconfirm
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'  --noconfirm
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'  --noconfirm
 
 #Настройка Pacman.conf
 echo '[options]
@@ -106,9 +106,9 @@ Include = /etc/pacman.d/mirrorlist
 Server = https://repo.archlinuxcn.org/$arch
 SigLevel = Never
 #
-#[chaotic-aur]
-#Include = /etc/pacman.d/chaotic-mirrorlist
-#SigLevel = Never
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+SigLevel = Never
 #' > /etc/pacman.conf
 curl -O https://blackarch.org/strap.sh
 sed -i 's/pacman -S --noconfirm --needed blackarch-officials/ /g' ./strap.sh
