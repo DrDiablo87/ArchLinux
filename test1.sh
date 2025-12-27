6cc2a87c2d102b85edd738c6382e43b9b6b824f9
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2025-12-27 20:53:09 +0300

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -74,8 +74,8 @@ systemctl enable --now systemd-resolved.service
 systemctl enable --now NetworkManager.service
 ping -c 5 google.com
 
-#pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-#pacman-key --lsign-key 3056513887B78AEB
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'  --noconfirm
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'  --noconfirm
 
