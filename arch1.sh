58e041c9b63cda8fcebea0bf1d9629085c906be2
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2025-12-28 23:56:17 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -130,8 +130,8 @@ echo -e '
 echo -e '\033[32m'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-#arch-chroot /mnt sh -c "$(curl -fsSL git.io/2.sh)"
-arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/DrDiablo87/ArchLinux/refs/heads/master/arch2.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/2.sh)"
+#arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/DrDiablo87/ArchLinux/refs/heads/master/arch2.sh)"
 systemctl reboot
 
 elif [[ $OS == 2 ]]; then
@@ -182,8 +182,8 @@ echo -e '
 '
 echo -e '\033[32m'
 genfstab -pU /mnt >> /mnt/etc/fstab
-#arch-chroot /mnt sh -c "$(curl -fsSL git.io/2.sh)"
-arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/DrDiablo87/ArchLinux/refs/heads/master/arch2.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/2.sh)"
+#arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/DrDiablo87/ArchLinux/refs/heads/master/arch2.sh)"
 systemctl reboot --firmware-setup
   
 fi 
