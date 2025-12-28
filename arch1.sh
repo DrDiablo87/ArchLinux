4a5e434d0a655e307eda40ba8c20164b9cc213b6
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2025-12-29 00:40:04 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -130,7 +130,7 @@ echo -e '
 echo -e '\033[32m'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL git.io/2.sh)"
+arch-chroot -S /mnt sh -c "$(curl -fsSL git.io/2.sh)"
 #arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/DrDiablo87/ArchLinux/refs/heads/master/arch2.sh)"
 systemctl reboot
 
