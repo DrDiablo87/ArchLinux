260eecda580cd2c31e2f0e98bf1e3e95b234b5b1
DrDiablo87 <32373275+DrDiablo87@users.noreply.github.com>
2025-12-29 03:01:16 +0300

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -3,7 +3,7 @@
 echo -e '\033[32m'
 
 #Прописываем имя компьютера
-echo $hostname > /etc/hostname
+echo DrDiablo > /etc/hostname
 ln -svf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc
 
@@ -79,7 +79,7 @@ options root=/dev/nvme0n1p2 rw rootflags=subvol=@ #quiet' > /boot/loader/entries
 fi
 
 #Добавляем пользователя
-useradd -m -g users -G audio,games,video,storage,wheel -s /bin/bash $username
+useradd -m -g users -G audio,games,video,storage,wheel -s /bin/bash Diablo
 
 echo -e '
 
@@ -88,17 +88,17 @@ echo -e '
 echo -e '\033[32m'
 
 echo -e "$rootpass\n$rootpass" | passwd
-echo -e "$userpass\n$userpass" | passwd $username
+echo -e "$userpass\n$userpass" | passwd Diablo
 
 echo -e '\033[32m'
 #Устанавливаем SUDO
 echo 'root ALL=(ALL) ALL
-'$username' ALL=(ALL) ALL
+Diablo ALL=(ALL) ALL
 %wheel ALL=(ALL) ALL
-'$username' ALL = NOPASSWD: /bin/systemctl
+Diablo ALL = NOPASSWD: /bin/systemctl
 Defaults env_reset, timestamp_timeout=30' > /etc/sudoers
 
-git clone https://github.com/DrDiablo87/ArchLinux.git /home/$username/ArchLinux
+git clone https://github.com/DrDiablo87/ArchLinux.git /home/Diablo/ArchLinux
 systemctl enable --now systemd-networkd.service
 systemctl enable --now systemd-resolved.service
 
@@ -110,7 +110,7 @@ pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.
 #Настройка Pacman.conf
 echo '[options]
 #
-CacheDir = /home/'$username'/Package/
+CacheDir = /home/Diablo/Package/
 HoldPkg = pacman glibc
 #
 Architecture = auto
@@ -156,7 +156,7 @@ sed -i 's!#PKGDEST=/home/packages!PKGDEST=~/Package!' /etc/makepkg.conf
 echo 'MAKEFLAGS="-j'$(nproc)'"
 BILDDIR=/temp/makepkg' >> /etc/makepkg.conf
                                              
-echo '127.0.1.1   '$hostname'.localdomain   '$hostname'
+echo '127.0.1.1   DrDiablo.localdomain   DrDiablo
 #127.0.0.1 localhost
 #::1       localhost
 127.0.0.1 admulti.com
@@ -198,18 +198,18 @@ echo -e '
 '
 echo -e '\033[32m'
 
-mkdir -p /home/$username/.config /home/$username/.local/share/konsole
+mkdir -p /home/Diablo/.config /home/Diablo/.local/share/konsole
 pacman -S zsh-theme-powerlevel10k awesome-terminal-fonts zsh-syntax-highlighting zsh-autosuggestions neofetch lsd bat fd --noconfirm
-usermod -s /usr/bin/zsh $username
+usermod -s /usr/bin/zsh Diablo
 usermod -s /usr/bin/zsh root
-cp /home/$username/ArchLinux/Package/.zshrc /home/$username/.zshrc
-cp /home/$username/ArchLinux/Package/zshrc /etc/zsh/zshrc
-mkdir -p /home/$username/.config/neofetch
-cp /home/$username/ArchLinux/Package/config.conf /home/$username/.config/neofetch/config.conf
-cp /home/$username/ArchLinux/Package/konsolerc /home/$username/.config/konsolerc
+cp /home/Diablo/ArchLinux/Package/.zshrc /home/Diablo/.zshrc
+cp /home/Diablo/ArchLinux/Package/zshrc /etc/zsh/zshrc
+mkdir -p /home/Diablo/.config/neofetch
+cp /home/Diablo/ArchLinux/Package/config.conf /home/Diablo/.config/neofetch/config.conf
+cp /home/Diablo/ArchLinux/Package/konsolerc /home/Diablo/.config/konsolerc
 mkdir -p /root/.config
-cp /home/$username/ArchLinux/Package/konsolerc /root/.config/konsolerc
-cp /home/$username/ArchLinux/KDE/.config/yt-dlp /home/$username/.config/yt-dlp
+cp /home/Diablo/ArchLinux/Package/konsolerc /root/.config/konsolerc
+cp /home/Diablo/ArchLinux/KDE/.config/yt-dlp /home/Diablo/.config/yt-dlp
 
 echo -e '
 
@@ -221,14 +221,14 @@ echo '[Theme]
 CursorTheme=breeze_cursors' > /etc/sddm.conf
 
 echo '[Autologin]
-User='$username'
+User=Diablo
 Session=plasma.desktop
 Numlock=on ' > /etc/sddm.conf.d/autologin.conf
 
 echo '[Autologin]
 Relogin=false
 Session=plasma
-User='$username'
+User=Diablo
 
 [General]
 HaltCommand=/usr/bin/systemctl poweroff
@@ -242,7 +242,7 @@ CursorTheme=breeze_cursors
 MaximumUid=60000
 MinimumUid=1000 ' > /etc/sddm.conf.d/kde_settings.conf
 
-chown -R $username:users /home/$username
+chown -R Diablo:users /home/Diablo
 
 #pacman -S torsocks tor i2pd torctl --noconfirm
 #cp /home/$username/ArchLinux/Package/i2pd.conf /etc/i2pd/i2pd.conf
@@ -274,7 +274,7 @@ systemctl enable fstrim.timer
 systemctl enable bluetooth.service
 systemctl mask systemd-tmpfiles-setup.service    # предотвращение создания проблемного снапшота
 btrfs subvolume delete /var/lib/machines         # удаление проблемного снапшота
-usermod -a -G wireshark $username
+usermod -a -G wireshark Diablo
 #===========================================================================================================================================================================================================
 
 echo -e '
@@ -287,11 +287,11 @@ pacman -S amarok taglib1 gst-libav gst-plugins-bad gst-plugins-good gst-plugins-
 
 pacman -S mpv --noconfirm
 mkdir /etc/mpv
-cp /home/$username/ArchLinux/Package/mpv.desktop /usr/share/applications/mpv.desktop
-cp /home/$username/ArchLinux/Package/mpv_single /usr/bin/mpv_single
+cp /home/Diablo/ArchLinux/Package/mpv.desktop /usr/share/applications/mpv.desktop
+cp /home/Diablo/ArchLinux/Package/mpv_single /usr/bin/mpv_single
 chmod +x /usr/bin/mpv_single
-cp /home/$username/ArchLinux/Package/mpv.conf /etc/mpv/mpv.conf
-cp /home/$username/ArchLinux/Package/input.conf /etc/mpv/input.conf
+cp /home/Diablo/ArchLinux/Package/mpv.conf /etc/mpv/mpv.conf
+cp /home/Diablo/ArchLinux/Package/input.conf /etc/mpv/input.conf
 
 echo -e '
 
@@ -301,7 +301,7 @@ echo -e '\033[32m'
 
 #cp /home/$username/.config/LiveWallpaper/archlinux-logo-dark.png /usr/share/sddm/themes/breeze/archlinux-logo-dark.png
 pacman -S virtualbox virtualbox-guest-utils --noconfirm
-cp /home/$username/ArchLinux/LiveWallpaper/Box.svg /usr/share/icons/breeze-dark/apps/48/Box.svg
+cp /home/Diablo/ArchLinux/LiveWallpaper/Box.svg /usr/share/icons/breeze-dark/apps/48/Box.svg
 #cp /home/$username/ArchLinux/LiveWallpaper/Box.svg /usr/share/icons/breeze/apps/48/Box.svg
 sed -i 's/Icon=virtualbox/Icon=Box/g' /usr/share/applications/virtualbox.desktop
 #SWAP
@@ -314,7 +314,7 @@ sed -i 's/Icon=virtualbox/Icon=Box/g' /usr/share/applications/virtualbox.desktop
 #swapon /swapfile
 #echo /swapfile none swap sw 0 0 | sudo tee -a /etc/fstab
 #echo 'vm.swappiness=10' > /etc/sysctl.d/99-sysctl.conf
-cp /home/$username/ArchLinux/Package/com.github.configurable-button.tar.xz /home/$username/com.github.configurable-button.tar.xz
+cp /home/Diablo/ArchLinux/Package/com.github.configurable-button.tar.xz /home/Diablo/com.github.configurable-button.tar.xz
 
 cp /usr/share/icons/breeze/apps/48/plasmavault.svg /usr/share/icons/breeze/apps/48/kleopatra.svg  
 cp /usr/share/icons/breeze/apps/48/plasmavault.svg /usr/share/icons/breeze-dark/apps/48/kleopatra.svg
@@ -323,36 +323,36 @@ cp /usr/share/icons/breeze-dark/preferences/32/preferences-desktop-keyboard.svg 
 sed -i 's/Icon=kleopatra/Icon=plasmavault/g' /usr/share/kio/servicemenus/kleopatra_decryptverifyfiles.desktop /usr/share/kio/servicemenus/kleopatra_signencryptfiles.desktop /usr/share/kio/servicemenus/kleopatra_signencryptfolders.desktop
 sed -i 's/use-ipv4=yes/use-ipv4=no/g' /etc/avahi/avahi-daemon.conf
 sed -i 's/use-ipv6=yes/use-ipv6=no/g' /etc/avahi/avahi-daemon.conf
-cp /home/$username/ArchLinux/Package/systemdgenie.mo /usr/share/locale/ru/LC_MESSAGES/systemdgenie.mo
-cp /home/$username/ArchLinux/Package/config.conf /home/$username/.config/neofetch/config.conf
+cp /home/Diablo/ArchLinux/Package/systemdgenie.mo /usr/share/locale/ru/LC_MESSAGES/systemdgenie.mo
+cp /home/Diablo/ArchLinux/Package/config.conf /home/Diablo/.config/neofetch/config.conf
 
-cp /home/$username/ArchLinux/LiveWallpaper/Airgeddon.svg /usr/share/icons/breeze-dark/apps/48/Airgeddon.svg
-cp /home/$username/ArchLinux/LiveWallpaper/Airgeddon.svg /usr/share/icons/breeze/apps/48/Airgeddon.svg
-cp /home/$username/ArchLinux/LiveWallpaper/Metasploit.svg /usr/share/icons/breeze-dark/apps/48/Metasploit.svg
-cp /home/$username/ArchLinux/LiveWallpaper/Metasploit.svg /usr/share/icons/breeze/apps/48/Metasploit.svg
-cp /home/$username/ArchLinux/KDE/.face.icon  /usr/share/plasma/avatars/Kurchatov.png
-cp /home/$username/ArchLinux/KDE/.local/share/applications/Archlinux-icon-crystal-64.png /usr/share/icons/breeze-dark/apps/48/Archlinux-icon-crystal-64.png
+cp /home/Diablo/ArchLinux/LiveWallpaper/Airgeddon.svg /usr/share/icons/breeze-dark/apps/48/Airgeddon.svg
+cp /home/Diablo/ArchLinux/LiveWallpaper/Airgeddon.svg /usr/share/icons/breeze/apps/48/Airgeddon.svg
+cp /home/Diablo/ArchLinux/LiveWallpaper/Metasploit.svg /usr/share/icons/breeze-dark/apps/48/Metasploit.svg
+cp /home/Diablo/ArchLinux/LiveWallpaper/Metasploit.svg /usr/share/icons/breeze/apps/48/Metasploit.svg
+cp /home/Diablo/ArchLinux/KDE/.face.icon  /usr/share/plasma/avatars/Kurchatov.png
+cp /home/Diablo/ArchLinux/KDE/.local/share/applications/Archlinux-icon-crystal-64.png /usr/share/icons/breeze-dark/apps/48/Archlinux-icon-crystal-64.png
 
-mkdir -p /home/$username/.config/autostart
+mkdir -p /home/Diablo/.config/autostart
 
 echo '[Desktop Entry]
-Exec=konsole -e sh /home/'$username'/ArchLinux/arch3.sh
+Exec=konsole -e sh /home/Diablo/ArchLinux/arch3.sh
 Icon=application-x-shellscript
 Name=arch3.sh
 Type=Application
-X-KDE-AutostartScript=true' > /home/$username/.config/autostart/arch3.sh.desktop
-chmod +x /home/$username/ArchLinux/arch3.sh
-chmod +x /home/$username/.config/autostart/arch3.sh.desktop
+X-KDE-AutostartScript=true' > /home/Diablo/.config/autostart/arch3.sh.desktop
+chmod +x /home/Diablo/ArchLinux/arch3.sh
+chmod +x /home/Diablo/.config/autostart/arch3.sh.desktop
 
 
 echo "
-tmpfs      /home/'$username'/.cache tmpfs rw 0 0
+tmpfs      /home/Diablo/.cache tmpfs rw 0 0
 tmpfs						/var/log	tmpfs	defaults,noatime 0 0
 tmpfs						/var/run	tmpfs	defaults,noatime 0 0
 tmpfs						/var/lock	tmpfs	defaults,noatime 0 0 " >> /etc/fstab
 
-rm -R /home/$username/.bash_logout /home/$username/.bash_profile /home/$username/.bashrc /home/$username/Package /var/cache/pacman/pkg
-chown -R $username:users /home/$username
+rm -R /home/Diablo/.bash_logout /home/Diablo/.bash_profile /home/Diablo/.bashrc /home/Diablo/Package /var/cache/pacman/pkg
+chown -R Diablo:users /home/Diablo
 
 #cp -Rf /home/$username/ArchLinux/KDE/.config /home/$username/.config
 #cp -Rf /home/$username/ArchLinux/KDE/. /root
